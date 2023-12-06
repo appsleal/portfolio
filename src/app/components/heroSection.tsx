d6cb541ff581cc46e417c348efc65e347a9b8464
Hermes Leal <appsleal@gmail.com>
2023-12-06 15:48:11 -0500

    add text hero style
    gradiants and br

--- src/app/components/heroSection.tsx
+++ src/app/components/heroSection.tsx
@@ -9,21 +9,26 @@ const HeroSection = () => {
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <div className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            <span>Hello, I'm </span>
-            <TypeAnimation
-              sequence={[
-                // Same substring at the start will only be typed out once, initially
-                "Hermes Leal",
-                1000, // wait 1s before replacing "Mice" with "Hamsters"
-                "Backend Developer",
-                1000,
-                "Mobile developer",
-                1000,
-              ]}
-              wrapper="span"
-              speed={50}
-              repeat={Infinity}
-            />{" "}
+            <h1 className=" text-white mb-4 font-extrabold">
+              <span className=" text-transparent bg-clip-text bg-gradient-to-r from-[#A71515] to-[#AC7C7C]">
+                Hello, I'm{" "}
+              </span>
+              <br />
+              <TypeAnimation
+                sequence={[
+                  // Same substring at the start will only be typed out once, initially
+                  "Hermes Leal",
+                  1000, // wait 1s before replacing "Mice" with "Hamsters"
+                  "A Backend dev",
+                  1000,
+                  "A Mobile dev",
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+              />
+            </h1>
           </div>
 
           <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl">
@@ -33,11 +38,13 @@ const HeroSection = () => {
             placeat ut! Ad.
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black mt-3">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#A71515] via-[#AC7C7C] to-[#7A3535] hover:bg-slate-200 text-white mt-3">
               Hire Me
             </button>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
-              Download CV
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#A71515] via-[#AC7C7C] to-[#7A3535] hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>{" "}
             </button>
           </div>
         </div>
